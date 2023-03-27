70f5080ab6d67d02cf294cc1f5551aec44a186e7
ipas isaiah <77514431+isaiah1456@users.noreply.github.com>
2023-03-28 00:54:54 +0300

    Landing background fixed

--- components/about/LandingBackground.vue
+++ components/about/LandingBackground.vue
@@ -1,10 +1,6 @@
 <template>
   <section>
-    <div class="background center">
-      <img src="~assets/img/first-screen.png" alt="">
-      <img src="~assets/img/front.png" alt="">
-      <img src="~assets/img/third-screen.png" alt="">
-    </div>
+    <img src="~assets/img/main.png" alt="" class="background">
     <p>
       All necessary functionality for NGOs who work in the ground fighting poverty and inequality.
       Workflow specialized on empowering people through knowledge and
@@ -24,9 +20,10 @@ section {
   text-align: center;
 
   .background {
-    display: flex;
     max-width: 100%;
-    margin-top: 2rem;
+    margin: 0 auto;
+    background-position: center;
+    background-size: cover;
 
     img {
       width: 100%;
@@ -35,13 +32,18 @@ section {
   }
 
   p {
-    font-size: 1.5rem;
-    padding: 2.5rem 2rem;
+    @apply text-2xl py-6 px-4 max-w-6xl;
     font-weight: 500;
     letter-spacing: 1.7px;
-    max-width: 72rem;
     text-align: center;
     margin: 0 auto;
+    font-weight: 500;
+  }
+
+  @media screen and (max-width: 768px) {
+    p {
+      @apply text-base w-full px-4;
+    }
   }
 }
 </style>
