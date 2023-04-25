2ddb3108499ba5323b00c313f76f0898428fa84c
ipas isaiah <77514431+isaiah1456@users.noreply.github.com>
2023-04-25 14:46:55 +0300

    infinite scroll animations icons

--- components/vv/about/Iconography.vue
+++ components/vv/about/Iconography.vue
@@ -2,6 +2,10 @@
   <vv-about-Iconography-Faces />
   <vv-about-Iconography-LineTwo />
   <vv-about-Iconography-Icons />
+  <vv-about-Iconography-Workshop />
+  <vv-about-Iconography-LineThree />
+  <vv-about-Iconography-Fourth />
+  <vv-about-Iconography-Tools />
 </template>
 
 <script setup lang="ts">
