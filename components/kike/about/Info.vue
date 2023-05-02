7c4e774180b1290a7da2034b07205dc861900b54
ipas isaiah <77514431+isaiah1456@users.noreply.github.com>
2023-05-02 16:38:49 +0300

    about page first section

---
+++ components/kike/about/Info.vue
@@ -0,0 +1,55 @@
+<template>
+  <section>
+    <div class="about-us">
+      <h2>
+        About
+      </h2>
+
+      <div class="info">
+        <p>
+          Kike Tordera, a skilled web developer based in Spain, has been creating
+          innovative and engaging websites for [number] years. With a passion for coding and a keen
+          eye for design, Kike Tordera is well-versed in a variety of programming languages, including HTML, CSS, JavaScript, and PHP.
+        </p>
+        <div class="pdf">
+          <img src="~assets/img/kk/kike.png" alt="">
+          <button type="submit">
+            <img src="~assets/svg/kk/resume.svg" alt="">
+            <span>Download Resume</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+section {
+    @apply w-full py-40 mx-auto px-4;
+    .about-us {
+        @apply max-w-6xl flex gap-8 flex-row mx-auto;
+         h2 {
+           @apply lg:text-4xl text-xl uppercase tracking-wider mb-4 w-1/2 my-8;
+                font-family: "Source Code Pro Bold";
+                font-weight: 600;
+                color: #CCCCCC;
+        }
+        .info {
+             @apply lg:w-3/4 w-full text-justify;
+            p {
+              @apply lg:text-lg  my-6 items-start;
+            }
+            button {
+                @apply text-base uppercase flex gap-3  py-4 my-5 text-center lg:w-auto w-full;
+                color: $background-color;
+                font-weight: 600;
+            }
+        }
+    }
+}
+
+</style>
---
+++ pages/about-us.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <kike-about-Info />
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
