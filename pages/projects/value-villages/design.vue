6343bc422bec6dc299256f52310fc7da5c5963cd
ipas isaiah <77514431+isaiah1456@users.noreply.github.com>
2023-05-25 13:22:05 +0300

    design page content added

--- pages/projects/value-villages/design.vue
+++ pages/projects/value-villages/design.vue
@@ -1,3 +1,5 @@
+<!-- eslint-disable max-len -->
+<!-- eslint-disable max-len -->
 <template>
   <div>
     <vv-general-NavBar />
@@ -15,10 +17,18 @@
     <vv-design-info
       class="mt-28"
       icon="alphabet-color.svg"
-      :text="text"
+      :text="text2"
       image="design-num.png"
       :left="true"
       title="Numerical alphabet"
+    />
+    <vv-design-info
+      class="mt-28"
+      icon="video-color.svg"
+      :text="text3"
+      image="vikapu.png"
+      :left="true"
+      title="Guides Video Courses"
     />
     <vv-design-Container>
       <vv-design-Performance />
@@ -31,6 +41,10 @@
 
 // eslint-disable-next-line max-len
 const text = 'When you start to create an interface adapted for illiteracy, you need to take normal routines that you do several times every day and reinvent it, giving them a better understanding and simplicity. One of these daily routines is introducing a password. Instead of a password, they can remember a story with 4 animals, and the iconography of the animals in convention with their photo, the software is able to give to the user the security they need to use the system.'
+// eslint-disable-next-line max-len
+const text2 = 'A numerical alphabet, like the roman but simplified, related to their currency to ensure that a person without the knowledge of the numbers will be able to understand how much money he/she needs to get paid.It can create any combination that the scope of the project can generate (covering all the money quantities) and is easily adaptable to any currency.'
+// eslint-disable-next-line max-len
+const text3 = 'Custom video tutorials, divided in steps for a guided and easy comprehension and with warnings about common mistakes to guide the process and avoid material waste.Also includes a set with icons and photos for a quick graphical resume of the tools and materials needed for the step.'
 </script>
 
 <style lang="scss" scoped>
